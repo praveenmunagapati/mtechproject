c4955927d7f8dc98d75b56944056654b38a80e99
jio <piratepieinc@gmail.com>
2024-03-26 09:08:56 +0530

    finalcode

---
+++ wildfire/my.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+import cv2
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing import image
+import matplotlib.pyplot as plt
+with tf.device('/cpu:0'):
+    model = tf.keras.models.load_model('mymodel.h5')
+model = keras.Sequential()
+model.add(keras.layers.Conv2D(32,(3,3),activation='relu',input_shape=(150,150,3)))
+model.add(keras.layers.MaxPool2D(2,2))
+model.add(keras.layers.Conv2D(64,(3,3),activation='relu'))
+model.add(keras.layers.MaxPool2D(2,2))
+model.add(keras.layers.Conv2D(128,(3,3),activation='relu'))
+model.add(keras.layers.MaxPool2D(2,2))
+model.add(keras.layers.Conv2D(128,(3,3),activation='relu'))
+model.add(keras.layers.MaxPool2D(2,2))
+model.add(keras.layers.Flatten())
+model.add(keras.layers.Dense(512,activation='relu'))
+model.add(keras.layers.Dense(1,activation='sigmoid'))
+model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+
+def predictImage(filename):
+
+    img1 = image.load_img(filename,target_size=(150,150))
+    plt.imshow(img1)
+    Y = image.img_to_array(img1)
+    X = np.expand_dims(Y,axis=0)
+    val = model.predict(X)
+    print(val)
+    if val < 0.4:
+        print("No Fire")
+    elif val > 0.8:
+        print("Fire")
+    return val
+
+predictImage("/workspaces/WildFire_Monitoring_Using_Satellite_Image/Flask_html/static/forest_fire.png")
